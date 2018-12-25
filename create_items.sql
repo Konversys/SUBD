e378af1b8e91e37278c3a6744d73f1ea2ad8e6fe
Maxim <butinmv9722@gmail.com>
2018-12-25 12:19:48 +0700

    Update create_items.sql

--- create_items.sql
+++ create_items.sql
@@ -11,3 +11,15 @@ BEGIN
     RETURN 1;
 END;
 $BODY$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION create_items_trigger() RETURNS TRIGGER AS $create_items_trigger$
+	BEGIN
+		perform create_items(NEW.id_group, NEW.id);
+		return new;
+    END;
+$create_items_trigger$ LANGUAGE plpgsql;
+
+CREATE TRIGGER create_items_trigger
+AFTER INSERT ON еrecord
+    FOR EACH ROW EXECUTE PROCEDURE create_items_trigger();
